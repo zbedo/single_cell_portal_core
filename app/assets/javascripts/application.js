9538066eed4ed9ac8fcdaa5af8b6819e6b7fcdeb
bistline <bistline@broadinstitute.org>
2016-05-20 10:51:30 -0400

    updating for running entirely in google

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require ckeditor/init
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require jquery-fileupload
 //= require spin.min
 //= require_tree .
 
