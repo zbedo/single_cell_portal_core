272a528a7fd8cb1cadc8b190f79fbada5c5cd4b1
bistline <bistline@broadinstitute.org>
2016-06-14 11:24:25 -0400

    updating configs for prod deployment

--- bin/tunnel_gcloud.sh
+++ bin/tunnel_gcloud.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcloud compute ssh single-cell-google --project broad-kdux-dev --zone us-central1-a --ssh-flag="-L" --ssh-flag="80:localhost:80" --ssh-flag="-L" --ssh-flag="443:localhost:443"
+sudo gcloud compute ssh singlecell-production --project broad-singlecellportal --zone us-central1-a --ssh-flag="-L" --ssh-flag="443:localhost:443"
