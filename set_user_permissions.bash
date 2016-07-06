48743c55b24c14828e570adf0f5b5e4fc7b00d75
bistline <bistline@broadinstitute.org>
2016-07-06 11:42:02 -0400

    only fix permissions in prod

--- set_user_permissions.bash
+++ set_user_permissions.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e # fail on any error
+if [[ $PASSENGER_APP_ENV = "production" ]]
+then
+	set -e # fail on any error
 
-echo '*** SETTING UID AND GID TO MATCH HOST VOLUMES ***'
-TARGET_UID=$(stat -c "%u" /home/app/webapp)
-echo '-- Setting app user to use uid '$TARGET_UID
-usermod -o -u $TARGET_UID app || true
-TARGET_GID=$(stat -c "%g" /home/app/webapp)
-echo '-- Setting app group to use gid '$TARGET_GID
-groupmod -o -g $TARGET_GID app || true
+	echo '*** SETTING UID AND GID TO MATCH HOST VOLUMES ***'
+	TARGET_UID=$(stat -c "%u" /home/app/webapp)
+	echo '-- Setting app user to use uid '$TARGET_UID
+	usermod -o -u $TARGET_UID app || true
+	TARGET_GID=$(stat -c "%g" /home/app/webapp)
+	echo '-- Setting app group to use gid '$TARGET_GID
+	groupmod -o -g $TARGET_GID app || true
+fi
