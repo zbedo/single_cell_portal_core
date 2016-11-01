0e1dace105cba6f2ef6fee4d517d2f8af689269f
bistline <bistline@broadinstitute.org>
2016-11-01 10:45:58 -0400

    adding longer timeout for long-running parse jobs, restricting Ckeditor asset browsing by user

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,7 +1,7 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_mongoid_attached_file :data,
-                            :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                            :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                            :url  => "/single_cell/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                            :path => ":rails_root/public/single_cell/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                             :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_size :data, :less_than => 2.megabytes
