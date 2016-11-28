5b134c05dbe40308663e46f69efb13eaaa774419
bistline <bistline@broadinstitute.org>
2016-11-28 12:29:20 -0500

    fixing error when renaming studies - force update of study_file.url_safe_name

--- app/models/study.rb
+++ app/models/study.rb
@@ -162,7 +162,15 @@ class Study
     if self.single_cells.empty?
       0
     else
-      self.single_cells.uniq!(&:name).count
+      if self.expression_matrix_file.nil?
+        self.single_cells.uniq!(&:name).count
+      else
+        file = File.open(self.expression_matrix_file.upload.path)
+        cells = file.readline.split("\t")
+        file.close
+        cells.shift
+        cells.size
+      end
     end
   end
 
