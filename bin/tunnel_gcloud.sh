175e60fd9ddecedaf345518d2f0140d33951cb78
bistline <bistline@broadinstitute.org>
2017-01-11 11:16:38 -0500

    adding config and ssh options for staging env

--- bin/tunnel_gcloud.sh
+++ bin/tunnel_gcloud.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-gcloud compute ssh singlecell-production --project broad-singlecellportal --zone us-central1-a
+# usage error message
+usage=$(
+cat <<EOF
+$0 [OPTION]
+-e VALUE	set the environment, defaults to 'production' (determines which VM to ssh into).
+-H COMMAND	print this text
+EOF
+)
+
+# set variables & defaults
+ENV="production"
+while getopts "e:H" OPTION; do
+case $OPTION in
+	e)
+		ENV="$OPTARG"
+		;;
+	H)
+		echo "$usage"
+		exit 0
+		;;
+	*)
+    echo "unrecognized option"
+    echo "$usage"
+    ;;
+	esac
+done
+
+gcloud compute ssh singlecell-$ENV --project broad-singlecellportal --zone us-central1-a
