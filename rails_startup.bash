a835085ac68138d58123b12c6718589d3242bd14
bistline <bistline@broadinstitute.org>
2017-02-14 10:32:26 -0500

    fixing disk email cron to run as root

--- rails_startup.bash
+++ rails_startup.bash
@@ -18,10 +18,11 @@ then
     sudo -E -u app -H bin/delayed_job start $PASSENGER_APP_ENV -n 4
     echo "*** ADDING CRONTAB TO CHECK DELAYED_JOB ***"
     echo "* * * * */15 /home/app/webapp/bin/job_monitor.rb $PASSENGER_APP_ENV" | crontab -u app -
-    echo "*** ADDING DAILY ADMIN DISK MONITOR EMAIL ***"
-    (crontab -l -u app && echo "* 3 * * * /home/app/webapp/bin/rails runner -e $PASSENGER_APP_ENV \"SingleCellMailer.daily_disk_status.deliver\"") | crontab -u app -
     echo "*** COMPLETED ***"
 fi
+echo "*** ADDING DAILY ADMIN DISK MONITOR EMAIL ***"
+echo "* 3 * * * /home/app/webapp/bin/rails runner -e $PASSENGER_APP_ENV \"SingleCellMailer.daily_disk_status.deliver\"" | crontab -u root -
+echo "*** COMPLETED ***"
 echo "*** REINDEXING COLLECTIONS ***"
 sudo -E -u app -H bundle exec rake RAILS_ENV=$PASSENGER_APP_ENV db:mongoid:create_indexes
 echo "*** COMPLETED ***"
