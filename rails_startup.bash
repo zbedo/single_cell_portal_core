4220367607ce16780503163e511c56236a237157
bistline <bistline@broadinstitute.org>
2017-02-16 13:45:02 -0500

    logging output of cron to logfile

--- rails_startup.bash
+++ rails_startup.bash
@@ -26,11 +26,11 @@ then
     echo "*** STARTING DELAYED_JOB ***"
     sudo -E -u app -H bin/delayed_job start $PASSENGER_APP_ENV -n 4
     echo "*** ADDING CRONTAB TO CHECK DELAYED_JOB ***"
-    echo "* * * * */15 . /home/app/.cron_env ; /home/app/webapp/bin/job_monitor.rb $PASSENGER_APP_ENV" | crontab -u app -
+    echo "* * * * */15 . /home/app/.cron_env ; /home/app/webapp/bin/job_monitor.rb $PASSENGER_APP_ENV >> /home/app/webapp/log/cron_out.log 2>&1" | crontab -u app -
     echo "*** COMPLETED ***"
 fi
 echo "*** ADDING DAILY ADMIN DISK MONITOR EMAIL ***"
-echo "* 3 * * * . /root/.cron_env ; /home/app/webapp/bin/rails runner -e $PASSENGER_APP_ENV \"SingleCellMailer.daily_disk_status.deliver\"" | crontab -u root -
+echo "* 3 * * * . /root/.cron_env ; /home/app/webapp/bin/rails runner -e $PASSENGER_APP_ENV \"SingleCellMailer.daily_disk_status.deliver\" >> /home/app/webapp/log/cron_out.log 2>&1" | crontab -u root -
 echo "*** COMPLETED ***"
 echo "*** REINDEXING COLLECTIONS ***"
 sudo -E -u app -H bundle exec rake RAILS_ENV=$PASSENGER_APP_ENV db:mongoid:create_indexes
