bdfafff8a06b494f6f8bf79295088ab505444939
bistline <bistline@broadinstitute.org>
2017-02-27 16:08:03 -0500

    removing unnecessary query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :populate_studies
-
-  # get all studies, needed for nav and home page downloads
-  def populate_studies
-    @studies = Study.order('name ASC')
-  end
 end
