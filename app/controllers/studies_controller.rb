2e46886e33c55bade8f33ba96faa567447d78412
bistline <bistline@broadinstitute.org>
2017-10-06 13:32:58 -0400

    finishing integration with compute permissions for workspace acls

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -115,7 +115,7 @@ class StudiesController < ApplicationController
             # skip validation as we don't wont to set the acl in FireCloud as it already exists
             new_share.save(validate: false)
             @permissions_changed << new_share
-          elsif portal_permissions[user] != StudyShare::PORTAL_ACL_MAP[permissions['accessLevel']]
+          elsif portal_permissions[user] != StudyShare::PORTAL_ACL_MAP[permissions['accessLevel']] && user != @study.user.email
             # share exists, but permissions are wrong
             share = @study.study_shares.detect(email: user)
             share.update(permission: StudyShare::PORTAL_ACL_MAP[permissions['accessLevel']])
