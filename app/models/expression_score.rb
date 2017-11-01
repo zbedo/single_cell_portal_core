1df8069bd987d03b906c48c810e2c9333b5bcdec
bistline <bistline@broadinstitute.org>
2017-11-01 10:40:26 -0400

    implementing basic statistical methods for expression heatmaps (z score, robust z score)

--- app/models/expression_score.rb
+++ app/models/expression_score.rb
@@ -33,10 +33,26 @@ class ExpressionScore
 
   # calculate a mean value for a given gene based on merged expression scores
   def self.mean(scores, cells)
-    sum = 0.0
-    cells.each do |cell|
-      sum += scores[cell].to_f
-    end
-    sum / cells.size
+    values = cells.map {|c| scores[c].to_f}
+    values.mean
+  end
+
+  # calculate a z-score for every entry (subtract the mean, divide by the standard deviation)
+  def self.z_score(scores, cells)
+    values = cells.map {|c| scores[c].to_f}
+    mean = values.mean
+    stddev = values.stdev
+    values.map {|v| (v - mean) / stddev}
+  end
+
+  # calculate a robust z-score for every entry (subtract the median, divide by the absolute median deviation)
+  def self.robust_z_score(scores, cells)
+    values = cells.map {|c| scores[c].to_f}
+    sorted_values = values.sort
+    len = sorted_values.length
+    median = (sorted_values[(len - 1) / 2] + sorted_values[len / 2]) / 2.0
+    deviations = values.map {|v| median - v}
+    abs_median_dev = deviations.reduce(:+) / values.size
+    values.map {|v| (v - median) / abs_median_dev}
   end
 end
