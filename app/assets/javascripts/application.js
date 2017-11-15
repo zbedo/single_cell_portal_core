7627231cacdbd65283f0bd16b9c3339111dc2e6f
bistline <bistline@broadinstitute.org>
2017-11-15 13:12:02 -0500

    formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -249,12 +249,12 @@ function enableDefaultActions() {
     $('form').on('focus', 'input[type=number]', function (e) {
         $(this).on('mousewheel.disableScroll', function (e) {
             e.preventDefault()
-        })
+        });
     });
 
     $('form').on('blur', 'input[type=number]', function (e) {
         $(this).off('mousewheel.disableScroll')
-    })
+    });
 }
 
 var stickyOptions = {
