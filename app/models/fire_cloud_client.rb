69fbc812258644cd66eaf339016421e7f2f58402
bistline <bistline@broadinstitute.org>
2017-12-12 10:20:10 -0500

    changing api_available? to use api_status endpoint, only making compute check on showing study overview when api is available

--- app/models/fire_cloud_client.rb
+++ app/models/fire_cloud_client.rb
@@ -274,10 +274,13 @@ class FireCloudClient < Struct.new(:user, :project, :access_token, :api_root, :s
 	# * *return*
 	#   - +Boolean+ indication of FireCloud current status
 	def api_available?
-		path = self.api_root + '/health'
 		begin
-			process_firecloud_request(:get, path)
-			true
+			response = self.api_status
+			if response.is_a?(Hash) && response['ok']
+				true
+			else
+				false
+			end
 		rescue => e
 			false
 		end
