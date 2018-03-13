cb70560f7f0d60409e909e882e09d86afe692732
bistline <bistline@broadinstitute.org>
2018-03-13 17:24:10 -0400

    adding new reports for pipelines

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -99,7 +99,7 @@ class ReportsController < ApplicationController
 
     # compute time-based breakdown of returning user counts
     @returning_users_by_week = {}
-    user_timepoints = ReportTimePoint.where(name: 'Weekly Returning Users').select {|tp| tp.date <= today}
+    user_timepoints = ReportTimePoint.where(name: 'Weekly Returning Users', :date.lte => today).take(32)
     user_timepoints.each do |timepoint|
       @returning_users_by_week[timepoint.date.to_s] = timepoint.value[:count]
     end
@@ -122,6 +122,67 @@ class ReportsController < ApplicationController
       @sorted_email_domain_dist[study_type] = {}
       sorted_domains.each do |domain|
         @sorted_email_domain_dist[study_type][domain] = user_study_email_dist[study_type][domain]
+      end
+    end
+
+    # get pipeline statistics
+    @pipeline_submissions = {}
+    @pipeline_runtimes = {}
+    AnalysisMetadatum.all.each do |analysis|
+      study = analysis.study
+      comp_method = analysis.payload['computational_method']
+      parts = comp_method.split('/')
+      pipeline_name = parts[parts.size - 2]
+      # get runtime info
+      tasks = analysis.payload['tasks']
+      start_times = []
+      stop_times = []
+      tasks.each do |task|
+        start_times << Time.parse(task['start_time'])
+        stop_times << Time.parse(task['stop_time'])
+      end
+      pipeline_start = start_times.sort.first
+      pipeline_end = stop_times.sort.last
+      runtime_in_min = (pipeline_end - pipeline_start) / 60.0
+      input_size = 0
+      @inputs = []
+      # determine how many input files there were
+      case pipeline_name
+        when /cell-ranger/
+          pipeline_inputs = analysis.payload['inputs'].detect {|input| input['name'] == 'cellranger.fastqs'}
+          @inputs = pipeline_inputs['value']
+        else
+          # make a guess, we have no idea
+          pipeline_inputs = analysis.payload['inputs'].select {|input| input['name'] =~ /fastq/}
+          pipeline_inputs.each do |input|
+            @inputs << input['value']
+          end
+          @inputs.flatten!
+      end
+      fastq_files = @inputs.map {|input| input.gsub(/gs:\/\/#{study.bucket_id}\//, '')}
+      # we need to now find the size of the files for the plot
+      fastq_files.each do |fastq|
+        directory = fastq.include?('/') ? fastq.split('/').first : '/'
+        study_directory = study.directory_listings.find_by(name: directory)
+        study_directory_file = study_directory.files.detect {|f| f['name'] == fastq}
+        input_size += study_directory_file['size']
+      end
+      if @pipeline_runtimes[pipeline_name].present?
+        @pipeline_runtimes[pipeline_name][:inputs] << input_size
+        @pipeline_runtimes[pipeline_name][:runtime] << runtime_in_min
+      else
+        @pipeline_runtimes[pipeline_name] = {inputs: [input_size], runtime: [runtime_in_min]}
+      end
+      # add run to count totals for this pipeline
+      date_bracket = pipeline_start.strftime('%Y-%m')
+      if @pipeline_submissions[pipeline_name].present?
+        if @pipeline_submissions[pipeline_name][date_bracket].present?
+          @pipeline_submissions[pipeline_name][date_bracket] += 1
+        else
+          @pipeline_submissions[pipeline_name][date_bracket] = 1
+        end
+      else
+        @pipeline_submissions[pipeline_name] = {"#{date_bracket}" => 1}
       end
     end
   end
