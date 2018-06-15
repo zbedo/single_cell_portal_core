b9745eca009ae422eb33a794756298ef9cd89fa6
bistline <bistline@broadinstitute.org>
2018-06-15 12:59:20 -0400

    making scatter plots auto-range to pick best render size, defaulting to viewport size - 250px for height to show entire plot in default view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -944,3 +944,8 @@ function gatherCoordinateMatrices() {
     });
     return matrices;
 }
+
+function calculatePlotViewport(target) {
+    var viewPort = $(window).height();
+    return viewPort - 250;
+}
