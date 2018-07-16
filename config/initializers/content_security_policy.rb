df1dcfdde987fd6be344565861104bd2bfb9becf
bistline <bistline@broadinstitute.org>
2018-07-16 17:25:51 -0400

    adding secure_headers for CSP management, TODO: update all script tags with nonce value

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,24 +4,47 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https, :unsafe_inline
-  policy.font_src    :self
-  policy.img_src     :self, :data, 'https://www.google-analytics.com'
-  policy.object_src  :none
-  policy.script_src  "https://#{ENV['PROD_HOSTNAME']}", :blob, :strict_dynamic, :unsafe_eval, 'https://cdn.plot.ly',
-                     'https://cdn.datatables.net', 'https://www.google-analytics.com', 'https://cdnjs.cloudflare.com',
-                     'https://maxcdn.bootstrapcdn.com', 'https://use.fontawesome.com'
-  policy.style_src   :self, :https, 'https://maxcdn.bootstrapcdn.com', :unsafe_inline
+#Rails.application.config.content_security_policy do |policy|
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-end
+#end
 
 # If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+      secure: true # mark all cookies as "Secure"
+  }
+  # Add "; preload" and submit the site to hstspreload.org for best protection.
+  config.hsts = "max-age=15768000; includeSubdomains; preload"
+  config.x_frame_options = "SAMEORIGIN"
+  config.x_content_type_options = "nosniff"
+  config.x_xss_protection = "1; mode=block"
+  config.x_permitted_cross_domain_policies = "none"
+  config.referrer_policy = %w(strict-origin-when-cross-origin)
+  config.csp = {
+      # "meta" values. these will shape the header, but the values are not included in the header.
+      preserve_schemes: true, # default: false. Schemes are removed from host sources to save bytes and discourage mixed content.
+
+      # directive values: these values will directly translate into source directives
+      default_src: %w('self'),
+      block_all_mixed_content: true, # see http://www.w3.org/TR/mixed-content/
+      frame_src: %w('self'), # if child-src isn't supported, the value for frame-src will be set.
+      font_src: %w('self' data:),
+      form_action: %w('self'),
+      img_src: %w('self' data: https://www.google-analytics.com),
+      manifest_src: %w('self'),
+      object_src: %w('none'),
+      plugin_types: %w(application/x-shockwave-flash),
+      script_src: %w('self' blob: 'unsafe-eval' 'strict-dynamic' https://cdn.plot.ly https://cdn.datatables.net https://www.google-analytics.com
+                      https://cdnjs.cloudflare.com https://maxcdn.bootstrapcdn.com https://use.fontawesome.com),
+      style_src: %w('self' https://maxcdn.bootstrapcdn.com 'unsafe-inline'),
+      upgrade_insecure_requests: true, # see https://www.w3.org/TR/upgrade-insecure-requests/
+  }
+
+end
