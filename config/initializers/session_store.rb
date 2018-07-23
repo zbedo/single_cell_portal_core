58607c60f4ecdd6cdd71df0f4feb41b9cd508e47
bistline <bistline@broadinstitute.org>
2018-07-23 17:09:39 -0400

    use random 16-digit string to invalidate all sessions on application boot

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_single_cell_portal_session'
+Rails.application.config.session_store :cookie_store, key: "_single_cell_portal_session_#{SecureRandom.hex(16)}"
