df058508249d83e3b24159e08cff6151604a9896
Yanay Rosen <yanay@broadinstitute.org>
2018-07-24 17:44:34 -0400

    Initial UI Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{2.days.to_i}"
