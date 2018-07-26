62f79bf366c5a2f1c836646db889d2c616a6f8c7
bistline <bistline@broadinstitute.org>
2018-07-26 14:44:14 -0400

    removing host header injection remediation patch

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module SingleCellPortal
 
     config.middleware.use Rack::Deflater
 
-    config.middleware.use StripXForwardedHost
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
