86282ea5f586fe1b227d643aaa9596b399acb0c5
bistline <bistline@broadinstitute.org>
2018-07-27 15:09:33 -0400

    adding ability to perform chunked download on files > 100MB
    Will perform a series of chunked downloads by sending range requests of 10MB and concatenating onto a local File object.  If under 100MB, the download is performed as a single call.

--- app/models/fire_cloud_client.rb
+++ app/models/fire_cloud_client.rb
@@ -1341,16 +1341,17 @@ class FireCloudClient < Struct.new(:user, :project, :access_token, :api_root, :s
 		end
 	end
 
-	# retrieve single file in a GCP bucket of a workspace and download locally to portal (likely for parsing)
-	#
-	# * *params*
-	#   - +workspace_namespace+ (String) => namespace of workspace
-	#   - +workspace_name+ (String) => name of workspace
-	#   - +filename+ (String) => name of file
-	#   - +destination+ (String) => destination path for downloaded file
-	#   - +opts+ (Hash) => extra options for signed_url, see
-	#     https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud-storage/v0.23.2/google/cloud/storage/file?method=signed_url-instance
-	#
+	# retrieve single file in a GCP bucket of a workspace and download locally to portal.  will perform a chunked download
+  # on files larger that 100 MB
+  #
+  # * *params*
+  #   - +workspace_namespace+ (String) => namespace of workspace
+  #   - +workspace_name+ (String) => name of workspace
+  #   - +filename+ (String) => name of file
+  #   - +destination+ (String) => destination path for downloaded file
+  #   - +opts+ (Hash) => extra options for signed_url, see
+  #     https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/google-cloud-storage/v0.23.2/google/cloud/storage/file?method=signed_url-instance
+  #
   # * *return*
   #   - +File+ object
   def download_workspace_file(workspace_namespace, workspace_name, filename, destination, opts={})
@@ -1364,7 +1365,23 @@ class FireCloudClient < Struct.new(:user, :project, :access_token, :api_root, :s
       directory = File.join(destination, path_parts)
       FileUtils.mkdir_p directory
     end
-    file.download end_path, opts
+    # determine if a chunked download is needed
+    if file.size > 100.megabytes
+      size_range = 0..file.size
+      local = File.new(end_path, 'a+')
+      size_range.each_slice(10000000) do |range|
+        range_req = range.first..range.last
+        merged_opts = opts.merge(range: range_req)
+        buffer = file.download merged_opts
+        buffer.rewind
+        local.write buffer.read
+        Rails.logger.info "Downloaded #{range_req.last} of #{file.size} bytes (#{(range_req.last / file.size.to_f * 100).round(2)}%) for #{filename} from #{workspace_namespace}/#{workspace_name}"
+      end
+      # return newly-opened file
+      File.open end_path
+    else
+      file.download end_path, opts
+    end
   end
 
   # generate a signed url to download a file that isn't public (set at study level)
