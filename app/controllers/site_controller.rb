e22a2aba8050067f6d1dc39b6c02d1ea45159337
bistline <bistline@broadinstitute.org>
2018-08-07 13:14:46 -0400

    integrating gene_id with autocomplete & expression data rendering

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -125,7 +125,7 @@ class SiteController < ApplicationController
 
     # if only one gene was searched for, make an attempt to load it and redirect to correct page
     if @terms.size == 1
-      @gene = load_best_gene_match(@study.genes.by_name(@terms.first, @study.expression_matrix_files.map(&:id)), @terms.first)
+      @gene = load_best_gene_match(@study.genes.by_name_or_id(@terms.first, @study.expression_matrix_files.map(&:id)), @terms.first)
       if @gene.empty?
         redirect_to merge_default_redirect_params(request.referrer, scpbr: params[:scpbr]),
                     alert: "No matches found for: #{@terms.first}." and return
@@ -149,7 +149,7 @@ class SiteController < ApplicationController
                                                                               heatmap_size: heatmap_size, colorscale: colorscale),
                                                 scpbr: params[:scpbr])
     else
-      redirect_to merge_default_redirect_params(view_gene_expression_heatmap_path(search: {genes: @terms.join(',')}, cluster: cluster,
+      redirect_to merge_default_redirect_params(view_gene_expression_heatmap_path(search: {genes: @terms.join(' ')}, cluster: cluster,
                                                                                   annotation: annotation, plot_type: plot_type,
                                                                                   boxpoints: boxpoints, heatmap_row_centering: heatmap_row_centering,
                                                                                   heatmap_size: heatmap_size, colorscale: colorscale),
@@ -375,7 +375,7 @@ class SiteController < ApplicationController
 
   # re-renders plots when changing cluster selection
   def render_gene_expression_plots
-    matches = @study.genes.by_name(params[:gene], @study.expression_matrix_files.map(&:id))
+    matches = @study.genes.by_name_or_id(params[:gene], @study.expression_matrix_files.map(&:id))
     subsample = params[:subsample].blank? ? nil : params[:subsample].to_i
     @gene = load_best_gene_match(matches, params[:gene])
     @y_axis_title = load_expression_axis_title
@@ -422,7 +422,7 @@ class SiteController < ApplicationController
   # renders gene expression plots, but from global gene search. uses default annotations on first render, but takes URL parameters after that
   def render_global_gene_expression_plots
     if check_xhr_view_permissions
-      matches = @study.genes.by_name(params[:gene], @study.expression_matrix_files.map(&:id))
+      matches = @study.genes.by_name_or_id(params[:gene], @study.expression_matrix_files.map(&:id))
       subsample = params[:subsample].blank? ? nil : params[:subsample].to_i
       @gene = load_best_gene_match(matches, params[:gene])
       @identifier = params[:identifier] # unique identifer for each plot for namespacing JS variables/functions (@gene.id)
@@ -536,7 +536,7 @@ class SiteController < ApplicationController
     # parse and divide up genes
     terms = parse_search_terms(:genes)
     @genes, @not_found = search_expression_scores(terms)
-    @gene_list = @genes.map{|gene| gene['name']}.join(',')
+    @gene_list = @genes.map{|gene| gene['name']}.join(' ')
     # load dropdown options
     @options = load_cluster_group_options
     @cluster_annotations = load_cluster_group_annotations
@@ -2000,7 +2000,7 @@ class SiteController < ApplicationController
     if sanitized_terms.is_a?(Array)
       sanitized_terms.map(&:strip)
     else
-      sanitized_terms.split(/[\n,]/).map(&:strip)
+      sanitized_terms.split(/[\n\s]/).map(&:strip)
     end
   end
 
@@ -2009,7 +2009,7 @@ class SiteController < ApplicationController
     genes = []
     matrix_ids = @study.expression_matrix_files.map(&:id)
     terms.each do |term|
-      matches = @study.genes.by_name(term, matrix_ids)
+      matches = @study.genes.by_name_or_id(term, matrix_ids)
       unless matches.empty?
         genes << load_best_gene_match(matches, term)
       end
--- db/migrate/20180806192849_set_gene_id_on_genes.rb
+++ db/migrate/20180806192849_set_gene_id_on_genes.rb
@@ -1,32 +1,9 @@
 class SetGeneIdOnGenes < Mongoid::Migration
   def self.up
-    study_files = StudyFile.where(file_type: 'MM Coordinate Matrix')
-    study_file_count = study_files.count
-    study_files.each_with_index do |study_file, study_file_index|
-      genes = Gene.where(study_id: study_file.study.id, study_file_id: study_file.id, gene_id: nil) # only process genes without a gene_id
-      gene_count = genes.count
-      genes.each_with_index do |gene, gene_index|
-        current_name = gene.name
-        new_name, gene_id = current_name.split('(').map {|entry| entry.strip.chomp(')')}
-        gene.update(name: new_name, searchable_name: new_name.downcase, gene_id: gene_id)
-        if (gene_index + 1) % 1000 == 0
-          Rails.logger.info "Processed #{gene_index + 1}/#{gene_count} records from file #{study_file_index + 1} of #{study_file_count}"
-        end
-      end
-    end
-    Rails.logger.info "Migration complete"
+    Gene.delay.add_gene_ids_to_genes # done as a background process to boot portal faster
   end
 
   def self.down
-    genes = Gene.where(:gene_id.nin => [nil])
-    gene_count = genes.count
-    genes.each_with_index do |gene, gene_index|
-      old_name = "#{gene.name} (#{gene.gene_id})"
-      gene.update(name: old_name, searchable_name: old_name.downcase, gene_id: nil)
-      if (gene_index + 1) % 1000 == 0
-        Rails.logger.info "Processed #{gene_index + 1}/#{gene_count} records"
-      end
-    end
-    Rails.logger.info "Rollback complete"
+    Gene.delay.remove_gene_ids_from_genes
   end
 end
