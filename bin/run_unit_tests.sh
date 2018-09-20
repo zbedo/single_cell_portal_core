80a261bce96f2330a37d1679e2a5993d71191cd6
bistline <bistline@broadinstitute.org>
2018-09-20 16:03:57 -0400

    adding new integration tests

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# script that is called when booting portal in test environment to run all unit tests in the correct order
+# script that is called when booting portal in test environment to run all unit tests in the correct order as some
+# tests change downstream behavior after they've run
 
 start=$(date +%s)
 echo "Seeding test database..."
@@ -10,16 +11,14 @@ ruby -I test test/integration/fire_cloud_client_test.rb
 ruby -I test test/integration/cache_management_test.rb
 ruby -I test test/api/studies_controller_test.rb
 ruby -I test test/api/study_files_controller_test.rb
+ruby -I test test/api/study_file_bundles_controller_test.rb
 ruby -I test test/api/study_shares_controller_test.rb
 ruby -I test test/api/directory_listings_controller_test.rb
 ruby -I test test/models/cluster_group_test.rb
 ruby -I test test/models/user_annotation_test.rb
 ruby -I test test/models/parse_utils_test.rb
 echo "Cleaning up..."
-/home/app/webapp/bin/rails runner -e test "DataArray.delete_all"
-/home/app/webapp/bin/rails runner -e test "Study.destroy_all"
-/home/app/webapp/bin/rails runner -e test "User.destroy_all"
-/home/app/webapp/bin/rails runner -e test "Delayed::Job.destroy_all"
+rake RAILS_ENV=test db:purge
 echo "Cleanup complete!"
 end=$(date +%s)
 difference=$(($end - $start))
