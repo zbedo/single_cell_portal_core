7b8ffff59360b03934375c6d6f6f6594921f347c
Eric Weitz <eweitz@broadinstitute.org>
2018-12-03 12:58:29 -0500

    Fix truncated track labels, upgrade Ideogram.js

--- app/assets/javascripts/scp-ideogram.js
+++ app/assets/javascripts/scp-ideogram.js
@@ -226,7 +226,7 @@ function initializeIdeogram(url) {
     organism: ideogramInferCnvSettings.organism.toLowerCase(),
     assembly: ideogramInferCnvSettings.assembly,
     chrHeight: 400,
-    dataDir: 'https://unpkg.com/ideogram@1.3.0/dist/data/bands/native/',
+    dataDir: 'https://unpkg.com/ideogram@1.4.0/dist/data/bands/native/',
     annotationsPath: url,
     annotationsLayout: 'heatmap',
     annotationsNumTracks: 3,
