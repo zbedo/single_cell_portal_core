b9a2fb181db4f91887391ad94e3a17d8926c4314
bistline <bistline@broadinstitute.org>
2019-01-23 15:44:36 -0500

    adding integration test for AnalysisConfiguration controller

--- test/controllers/analysis_configurations_controller_test.rb
+++ test/controllers/analysis_configurations_controller_test.rb
@@ -1,46 +1,56 @@
-require "test_helper"
+require "integration_test_helper"
 
-describe AnalysisConfigurationsController do
-  let(:analysis_configuration) { analysis_configurations :one }
+class AnalysisConfigurationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 
-  it "gets index" do
-    get analysis_configurations_url
-    value(response).must_be :success?
+  def setup
+    @test_user = User.find_by(email: 'testing.user@gmail.com')
+    @analysis_configuration = AnalysisConfiguration.first
+    auth_as_user(@test_user)
+    sign_in @test_user
   end
 
-  it "gets new" do
-    get new_analysis_configuration_url
-    value(response).must_be :success?
+  test 'should get index page' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+
+    get analysis_configurations_path
+    assert_response 200, 'Did not successfully load analysis configurations index page'
+    assert_select 'tr.analysis-configuration-entry', 1, 'Did not find any analysis configuration entries'
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
-  it "creates analysis_configuration" do
-    expect {
-      post analysis_configurations_url, params: { analysis_configuration: {  } }
-    }.must_change "AnalysisConfiguration.count"
-
-    must_redirect_to analysis_configuration_path(AnalysisConfiguration.last)
+  test 'should get detail page' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+    get analysis_configuration_path(@analysis_configuration)
+    assert_response 200, "Did not successfully load detail page for #{@analysis_configuration.identifier}"
+    assert_select 'table#analysis-parameters', 1, 'Did not find analysis parameters table'
+    assert_select 'tr.analysis-parameter-entry', 3, 'Did not find correct number of analysis parameter entries'
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
-  it "shows analysis_configuration" do
-    get analysis_configuration_url(analysis_configuration)
-    value(response).must_be :success?
-  end
-
-  it "gets edit" do
-    get edit_analysis_configuration_url(analysis_configuration)
-    value(response).must_be :success?
-  end
-
-  it "updates analysis_configuration" do
-    patch analysis_configuration_url(analysis_configuration), params: { analysis_configuration: {  } }
-    must_redirect_to analysis_configuration_path(analysis_configuration)
-  end
-
-  it "destroys analysis_configuration" do
-    expect {
-      delete analysis_configuration_url(analysis_configuration)
-    }.must_change "AnalysisConfiguration.count", -1
-
-    must_redirect_to analysis_configurations_path
+  test 'should create and delete analysis configuration' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+    analysis_configuration_params = {
+        analysis_configuration: {
+            namespace: 'unity-benchmark-test',
+            name: 'test-analysis',
+            snapshot: 1,
+            user_id: @test_user.id
+        }
+    }
+    post analysis_configurations_path, params: analysis_configuration_params
+    follow_redirect!
+    assert_response 200, 'Did not redirect successfully after creating analysis configuration'
+    get analysis_configurations_path
+    new_config = AnalysisConfiguration.find_by(namespace: 'unity-benchmark-test', name: 'test-analysis', snapshot: 1)
+    assert new_config.present?, 'Analysis configuration did not persist'
+    assert new_config.analysis_parameters.size == 2,
+           "Did not find correct number of parameters, expected 2 but found #{new_config.analysis_parameters.size}"
+    delete analysis_configuration_path(new_config)
+    follow_redirect!
+    assert_response 200, 'Did not successfully redirect after deleting analysis configuration'
+    assert AnalysisConfiguration.count == 1,
+           "Did not successfully delete analysis configuration, found #{AnalysisConfiguration.count} instead of 1"
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 end
