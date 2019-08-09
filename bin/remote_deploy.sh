6f828a17ae89de9e10da288fa7a763e24ecdf013
bistline <bistline@broadinstitute.org>
2019-08-09 12:32:13 -0400

    fixing quoting in remote_deploy

--- bin/remote_deploy.sh
+++ bin/remote_deploy.sh
@@ -44,7 +44,7 @@ function main {
 
     # run boot command
     echo "### Booting $PORTAL_CONTAINER ###"
-    run_command_in_deployment "bin/boot_docker -e '$PASSENGER_APP_ENV' -d '$DESTINATION_BASE_DIR' -h '$PROD_HOSTNAME' -N '$PORTAL_NAMESPACE' -m '$MONGO_LOCALHOST'"
+    run_command_in_deployment "bin/boot_docker -e $PASSENGER_APP_ENV -d $DESTINATION_BASE_DIR -h $PROD_HOSTNAME -N $PORTAL_NAMESPACE -m $MONGO_LOCALHOST"
     echo "### COMPLETED ###"
 
     # ensure portal is running
