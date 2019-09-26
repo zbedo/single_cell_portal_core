24ec2dda1f21ef8047b111ddc46b8a09ce53edee
bistline <bistline@broadinstitute.org>
2019-09-26 11:16:04 -0400

    addressing formatting in tcell_config, updating to 1.11.1

--- bin/configure_tcell.rb
+++ bin/configure_tcell.rb
@@ -7,14 +7,14 @@ require 'json'
 unless ENV['TCELL_AGENT_APP_ID'].nil? || ENV['TCELL_AGENT_API_KEY'].nil?
 
   tcell_config_json = {
-      "version": 1,
-      "applications": [
+      version: 1,
+      applications: [
           {
-              "app_id": "#{ENV['TCELL_AGENT_APP_ID']}",
-              "api_key": "#{ENV['TCELL_AGENT_API_KEY']}",
-              "tcell_api_url": "https://us.agent.tcell.insight.rapid7.com/api/v1",
-              "tcell_input_url": "https://us.input.tcell.insight.rapid7.com/api/v1",
-              "js_agent_api_base_url": "https://us.agent.tcell.insight.rapid7.com/api/v1"
+              app_id: "#{ENV['TCELL_AGENT_APP_ID']}",
+              api_key: "#{ENV['TCELL_AGENT_API_KEY']}",
+              tcell_api_url: "https://us.agent.tcell.insight.rapid7.com/api/v1",
+              tcell_input_url: "https://us.input.tcell.insight.rapid7.com/api/v1",
+              js_agent_api_base_url: "https://us.agent.tcell.insight.rapid7.com/api/v1"
           }
       ]
   }
