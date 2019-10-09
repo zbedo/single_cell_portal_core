721bc10fe1ee83f58def247487cb1101af18335d
bistline <bistline@broadinstitute.org>
2019-10-09 15:15:55 -0400

    forcing ToS acceptance through redirect

--- test/integration/tos_acceptance_test.rb
+++ test/integration/tos_acceptance_test.rb
@@ -11,6 +11,10 @@ class TosAcceptanceTest < ActionDispatch::IntegrationTest
   test 'should record user tos action' do
     puts "#{File.basename(__FILE__)}: #{self.method_name}"
     sign_in @test_user
+    # first log in and validate that the user is redirected to the ToS page
+    get site_path
+    follow_redirect!
+    assert path == accept_tos_path(@test_user.id), "Did not redirect to terms of service path, current path is #{path}"
     # first deny ToS and validate that user gets signed out
     post record_tos_action_path(id: @test_user.id, tos: {action: 'deny'})
     follow_redirect!
@@ -22,6 +26,9 @@ class TosAcceptanceTest < ActionDispatch::IntegrationTest
     follow_redirect!
     user_accepted = TosAcceptance.accepted?(@test_user)
     assert user_accepted, "Did not record user acceptance, acceptance shows: #{user_accepted}"
+    # now get another page and validate that redirect is no longer being enforced
+    get site_path
+    assert path == site_path, "Redirect still being enforced, expected path to be #{site_path} but found #{path}"
     puts "#{File.basename(__FILE__)}: #{self.method_name} completed!"
   end
 end
