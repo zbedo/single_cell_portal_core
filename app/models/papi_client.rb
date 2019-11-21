fd1c767aa166bea59b3a1e787ddd239c76bf05a8
Eric Weitz <eweitz@broadinstitute.org>
2019-11-21 11:07:11 -0500

    Add condition for --validation-convention flag

--- app/models/papi_client.rb
+++ app/models/papi_client.rb
@@ -230,7 +230,11 @@ class PapiClient < Struct.new(:project, :service_account_credentials, :service)
                       " --gene-file #{genes_file.gs_url} --barcode-file #{barcodes_file.gs_url}"
       end
     when 'ingest_cell_metadata'
-      command_line += " --cell-metadata-file #{study_file.gs_url} --ingest-cell-metadata --validate-convention"
+      if study_file.use_metadata_convention:
+        validate_convention = "--validate-convention"
+      else:
+        validate_convention = ""
+      command_line += " --cell-metadata-file #{study_file.gs_url} --ingest-cell-metadata #{validate_convention}"
     when 'ingest_cluster'
       command_line += " --cluster-file #{study_file.gs_url} --ingest-cluster"
     when 'ingest_subsample'
