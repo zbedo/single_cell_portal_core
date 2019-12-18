bf8ebd52289558f4e416da4f270ed341e6dacb98
Scott Longley <slongley@broadinstitute.org>
2019-12-18 12:40:00 -0500

    add back deprecated test we actually depend on

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -78,6 +78,7 @@ else
                     test/api/study_file_bundles_controller_test.rb
                     test/api/study_shares_controller_test.rb
                     test/api/directory_listings_controller_test.rb
+                    test/models/cluster_group_test.rb # deprecated, but needed to set up for user_annotation_test
                     test/models/user_annotation_test.rb
                     test/models/analysis_configuration_test.rb
   )
