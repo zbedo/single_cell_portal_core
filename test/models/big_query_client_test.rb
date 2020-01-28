5647b1f3c5c16d238b9360943605b8bb6024fd90
bistline <bistline@broadinstitute.org>
2020-01-28 17:18:43 -0500

    adding logging statements

--- test/models/big_query_client_test.rb
+++ test/models/big_query_client_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 class BigQueryClientTest < ActiveSupport::TestCase
 
-  test 'should instantiate client' do
-
+  test 'should instantiate client and assign attributes' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
     @bq = BigQueryClient.new
     assert_not_nil @bq
     assert_not_nil @bq.client
     assert_equal @bq.project, ENV['GOOGLE_CLOUD_PROJECT']
     assert_equal @bq.service_account_credentials, ENV['SERVICE_ACCOUNT_KEY']
-
+    puts "#{File.basename(__FILE__)}: #{self.method_name} completed!"
   end
 
 end
