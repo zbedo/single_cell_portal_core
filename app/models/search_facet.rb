3d7de70af7d889f967cd647efb9fc2314b8fcb66
bistline <bistline@broadinstitute.org>
2020-01-29 15:20:15 -0500

    addressing issue with array-based columns & DISTINCT queries, updating tests

--- app/models/search_facet.rb
+++ app/models/search_facet.rb
@@ -5,17 +5,20 @@ class SearchFacet
   extend ErrorTracker
 
   field :name, type: String
+  field :identifier, type: String
   field :filters, type: Array, default: []
   field :is_ontology_based, type: Boolean, default: false
+  field :ontology_urls, type: Array, default: []
   field :is_array_based, type: Boolean
   field :big_query_id_column, type: String
   field :big_query_name_column, type: String
   field :convention_name, type: String
   field :convention_version, type: String
 
-  validates_presence_of :name, :big_query_id_column, :big_query_name_column, :convention_name, :convention_version
+  validates_presence_of :name, :identifier, :big_query_id_column, :big_query_name_column, :convention_name, :convention_version
   validates_uniqueness_of :big_query_id_column, scope: [:convention_name, :convention_version]
-  before_create :set_is_array_based, unless: proc {|attributes| attributes[:is_array_based].present?}
+  validate :ensure_ontology_url_format, if: proc {|attributes| attributes[:is_ontology_based]}
+  before_create :set_is_array_based, if: proc {|attributes| attributes[:is_array_based].nil?}
 
   def self.big_query_dataset
     ApplicationController.big_query_client.dataset(CellMetadatum::BIGQUERY_DATASET)
@@ -39,10 +42,25 @@ class SearchFacet
     end
   end
 
-  # return a formatted object to use to render
+  # update all search facet filters after BQ update
+  def self.update_all_facet_filters
+    self.all.each do |facet|
+      Rails.logger.info "Updating #{facet.name} filter values"
+      updated = facet.update_filter_values!
+      if updated
+        Rails.logger.info "Update to #{facet.name} complete!"
+      else
+        Rails.logger.error "Update to #{facet.name} failed"
+      end
+    end
+  end
+
+  # return a formatted object to use to render search UI component
   def facet_config
     {
         name: self.name,
+        id: self.identifier,
+        links: self.ontology_urls,
         filters: self.filters
     }
   end
@@ -50,17 +68,23 @@ class SearchFacet
   # retrieve unique values from BigQuery and format an array of hashes with :name and :id values to populate :filters attribute
   def get_unique_filter_values
     Rails.logger.info "Updating filter values for SearchFacet: #{self.name} using id: #{self.big_query_id_column} and name: #{self.big_query_name_column}"
-    # generate DISTINCT sub-query strings for each column
-    subqueries = {self.big_query_id_column => "", self.big_query_name_column => ""}
-    subqueries.each_key do |bq_col|
-      subqueries[bq_col] = self.generate_distinct_sub_query(bq_col)
+    queries = []
+    if self.is_array_based
+      queries << self.generate_array_query(self.big_query_id_column, 'id')
+      queries << self.generate_array_query(self.big_query_name_column, 'name')
+    else
+      queries << self.generate_non_array_query
     end
-    query_string = "SELECT DISTINCT #{subqueries[self.big_query_id_column]} as id, #{subqueries[self.big_query_name_column]} as name FROM #{CellMetadatum::BIGQUERY_TABLE}"
     begin
-      SearchFacet.big_query_dataset.query(query_string)
+      results = []
+      queries.each do |query_string|
+        Rails.logger.info "Executing query: #{query_string}"
+        results << SearchFacet.big_query_dataset.query(query_string)
+      end
+      return assemble_filters_array(results)
     rescue => e
       Rails.logger.error "Error retrieving unique values for #{CellMetadatum::BIGQUERY_TABLE}: #{e.class.name}:#{e.message}"
-      error_context = ErrorTracker.format_extra_context({query_string: query_string})
+      error_context = ErrorTracker.format_extra_context({queries: queries})
       ErrorTracker.report_exception(e, nil, error_context)
       []
     end
@@ -76,14 +100,33 @@ class SearchFacet
     end
   end
 
-  # generate a sub-query string to use when trying to extract DISTINCT values from a column
-  # depends on whether column is array-based, which requires calling UNNEST()
-  # otherwise, sub-query is simply the column name as we can call DISTINCT on non-array columns
-  def generate_distinct_sub_query(column_name)
-    if self.is_array_based
-      "(SELECT * FROM UNNEST(#{column_name}))"
+  # generate a single query to get DISTINCT values from an array-based column, preserving order
+  # this way we can do two queries, and stitch them together in a hash as the order will be the same
+  # e.g. IDs will line up with NAMEs
+  def generate_array_query(column_name, identifier)
+    "SELECT DISTINCT #{identifier} FROM(SELECT array_col AS #{identifier}, " + \
+    "FROM #{CellMetadatum::BIGQUERY_TABLE}, UNNEST(#{column_name}) AS array_col " + \
+    "WITH OFFSET AS offset ORDER BY offset)"
+  end
+
+  # generate query string to retrieve distinct values for non-array based facets
+  def generate_non_array_query
+    "SELECT DISTINCT #{self.big_query_id_column} AS id, #{self.big_query_name_column} AS name FROM #{CellMetadatum::BIGQUERY_TABLE}"
+  end
+
+  # stitch together results into the formatted filters array based on whether or not we ran one or two queries
+  def assemble_filters_array(results_array)
+    if results_array.size == 1
+      results_array.first # already in the correct form, so just return
     else
-      column_name
+      # we have two hashes we need to merge together
+      filters_array = []
+      id_array = results_array.first
+      name_array = results_array.last
+      id_array.each_with_index do |filter_hash, index|
+        filters_array << filter_hash.merge(name_array[index])
+      end
+      filters_array
     end
   end
 
@@ -93,5 +136,30 @@ class SearchFacet
   def set_is_array_based
     column_schema = SearchFacet.get_table_schema(column_name: self.big_query_id_column)
     self.is_array_based = column_schema[:data_type].include?('ARRAY')
+  end
+
+  # custom validator for checking ontology_urls array
+  def ensure_ontology_url_format
+    if self.ontology_urls.blank?
+      errors.add(:ontology_urls, "cannot be empty if SearchFacet is ontology-based")
+    else
+      self.ontology_urls.each do |ontology_url|
+        # check that entry is a Hash with :name and :url field
+        unless ontology_url.is_a?(Hash) && ontology_url.with_indifferent_access.keys.sort == %w(name url)
+          errors.add(:ontology_urls, "contains a misformed entry: #{ontology_url}. Must be a Hash with a :name and :url field")
+        end
+        santized_url = ontology_url.with_indifferent_access
+        unless url_valid?(santized_url[:url])
+          errors.add(:ontology_urls, "contains an invalid URL: #{santized_url[:url]}")
+        end
+      end
+    end
+  end
+
+  # a URL may be technically well-formed but may
+  # not actually be valid, so this checks for both.
+  def url_valid?(url)
+    url = URI.parse(url) rescue false
+    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
   end
 end
--- test/models/search_facet_test.rb
+++ test/models/search_facet_test.rb
@@ -3,18 +3,7 @@ require "test_helper"
 class SearchFacetTest < ActiveSupport::TestCase
 
   def setup
-    @search_facet = SearchFacet.new(
-        name: 'Species',
-        filters: [
-            {id: 'NCBITaxon_9606', name: 'Homo sapiens'}
-        ],
-        is_ontology_based: true,
-        is_array_based: false,
-        big_query_id_column: 'species',
-        big_query_name_column: 'species__ontology_label',
-        convention_name: 'alexandria_convention',
-        convention_version: '1.1.3'
-    )
+    @search_facet = SearchFacet.first
 
     # filter_results to return from mock call to BigQuery
     @filter_results = [
@@ -33,10 +22,8 @@ class SearchFacetTest < ActiveSupport::TestCase
     mock.expect :query, @filter_results, [String]
 
     SearchFacet.stub :big_query_dataset, mock do
-
-      @search_facet.update_filter_values!
-
-      assert_equal @filter_results, @search_facet.filters
+      filters = @search_facet.get_unique_filter_values
+      assert_equal @filter_results, filters
       mock.verify
     end
 
@@ -48,35 +35,64 @@ class SearchFacetTest < ActiveSupport::TestCase
     puts "#{File.basename(__FILE__)}: #{self.method_name}"
 
     config = @search_facet.facet_config
-    assert_equal 'Species', config[:name]
-    assert_equal [{id: 'NCBITaxon_9606', name: 'Homo sapiens'}], config[:filters]
+    assert_equal @search_facet.name, config[:name]
+    assert_equal @search_facet.identifier, config[:id]
+    assert_equal @search_facet.ontology_urls, config[:links]
+    assert_equal @search_facet.filters, config[:filters]
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} complete!"
   end
 
-  # should correctly generate sub-query string for extracting distinct values from BQ
-  test 'should generate correct distinct sub-query' do
+  # should generate correct kind of query for DISTINCT filters based on array/non-array columns
+  test 'should generate correct distinct queries' do
     puts "#{File.basename(__FILE__)}: #{self.method_name}"
 
-    sub_query = @search_facet.generate_distinct_sub_query(@search_facet.big_query_id_column)
-    assert_equal @search_facet.big_query_id_column, sub_query
-
-    array_based_facet = SearchFacet.new(
+    non_array_query = @search_facet.generate_non_array_query
+    assert_match /DISTINCT #{@search_facet.big_query_id_column}/, non_array_query,
+                 "Non-array query did not contain correct DISTINCT clause: #{non_array_query}"
+    array_facet = SearchFacet.new(
         name: 'Disease',
-        filters: [
-            {id: 'MONDO_0006052', name: 'pulmonary tuberculosis'}
-        ],
+        identifier: 'disease',
         is_ontology_based: true,
         is_array_based: true,
+        filters: [],
+        ontology_urls: [
+            {name: 'Monarch Disease Ontology', url: 'https://www.ebi.ac.uk/ols/ontologies/mondo'},
+            {name: 'Phenotype And Trait Ontology', url: 'https://www.ebi.ac.uk/ols/ontologies/pato'}
+        ],
         big_query_id_column: 'disease',
         big_query_name_column: 'disease__ontology_label',
         convention_name: 'alexandria_convention',
         convention_version: '1.1.3'
     )
+    column = array_facet.big_query_id_column
+    identifier = 'id'
+    array_query = array_facet.generate_array_query(column, identifier)
+    assert_match /SELECT DISTINCT #{identifier}.*UNNEST\(#{column}\)/, array_query,
+                 "Array query did not correctly name identifier or unnest column: #{array_query}"
 
-    array_sub_query = array_based_facet.generate_distinct_sub_query(array_based_facet.big_query_id_column)
-    expected_sub_query = "(SELECT * FROM UNNEST(#{array_based_facet.big_query_id_column}))"
-    assert_equal expected_sub_query, array_sub_query
+    puts "#{File.basename(__FILE__)}: #{self.method_name} complete!"
+  end
+
+  # should validate search facet correctly, especially links to external ontologies
+  test 'should validate search_facet including ontology urls' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+
+    assert @search_facet.valid?, "Testing search facet did not validate: #{@search_facet.errors.full_messages}"
+    invalid_facet = SearchFacet.new
+    assert_not invalid_facet.valid?, 'Did not correctly find validation errors on empty facet'
+    expected_error_count = 6
+    invalid_facet_error_count = invalid_facet.errors.size
+    assert_equal expected_error_count, invalid_facet_error_count,
+           "Did not find correct number of errors; expected #{expected_error_count} but found #{invalid_facet_error_count}"
+    @search_facet.ontology_urls = []
+    assert_not @search_facet.valid?, 'Did not correctly find validation errors on invalid facet'
+    assert_equal @search_facet.errors.to_hash[:ontology_urls].first,
+                 'cannot be empty if SearchFacet is ontology-based'
+    @search_facet.ontology_urls = [{name: 'My Ontology', url: 'not a url'}]
+    assert_not @search_facet.valid?, 'Did not correctly find validation errors on invalid facet'
+    assert_equal @search_facet.errors.to_hash[:ontology_urls].first,
+                 'contains an invalid URL: not a url'
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} complete!"
   end
