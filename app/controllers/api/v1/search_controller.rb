809961a59d5619dc934bf37fff763e78eb76d7cc
bistline <bistline@broadinstitute.org>
2020-01-30 17:47:01 -0500

    integrating keyword & faceted search in API

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -6,6 +6,7 @@ module Api
 
       before_action :set_current_api_user!
       before_action :set_search_facet, only: :facet_filters
+      before_action :set_search_facets_and_filters, only: :index
 
       swagger_path '/search' do
         operation :get do
@@ -49,12 +50,37 @@ module Api
                 key :type, :string
                 key :title, 'Keywords used in search'
               end
+              property :page do
+                key :type, :string
+                key :title, 'Pagination control'
+              end
               property :facets do
                 key :type, :array
-                key :title, 'Array of facets/filters used in search'
+                key :title, 'SearchFacets'
+                key :description, 'Array of facets/filters used in search'
                 items do
-                  key :title, 'SearchFacetQuery'
-                  key :'$ref', :SearchFacetQuery
+                  key :type, :object
+                  key :title, 'SearchFacet'
+                  property :id do
+                    key :type, :string
+                    key :description, 'ID of facet'
+                  end
+                  property :filters do
+                    key :type, :array
+                    key :description, 'Matching filters'
+                    items do
+                      key :type, :object
+                      key :title, 'Filter'
+                      property :name do
+                        key :type, :string
+                        key :description, 'Display value of filter'
+                      end
+                      property :id do
+                        key :type, :string
+                        key :description, 'ID value of filter'
+                      end
+                    end
+                  end
                 end
               end
               property :studies do
@@ -85,11 +111,23 @@ module Api
           search_terms = sanitize_search_values(params[:terms])
           # determine if search values contain possible study accessions
           possible_accessions = StudyAccession.sanitize_accessions(search_terms.split)
-          @studies = @viewable.any_of({:$text => {:$search => search_terms}}, {:accession.in => possible_accessions}).
-              paginate(page: params[:page], per_page: Study.per_page)
+          @studies = @viewable.any_of({:$text => {:$search => search_terms}}, {:accession.in => possible_accessions})
         else
-          @studies = @viewable.paginate(page: params[:page], per_page: Study.per_page)
+          @studies = @viewable
         end
+
+        # only call BigQuery if list of possible studies is larger than 0 and we have matching facets to use
+        if @studies.count > 0 && @facets.any?
+          @big_query_search = generate_bq_query_string(@facets)
+          Rails.logger.info "Searching BigQuery using facet-based query: #{@big_query_search}"
+          raw_accessions = ApplicationController.big_query_client.dataset(CellMetadatum::BIGQUERY_DATASET).query @big_query_search
+          job_id = raw_accessions.job_gapi.job_reference.job_id
+          @convention_accessions = raw_accessions.map {|match| match[:study_accession]}
+          Rails.logger.info "Found #{@convention_accessions.count} matching studies from BQ job #{job_id}: #{@convention_accessions}"
+          @studies = @studies.where(:accession.in => @convention_accessions)
+        end
+        # paginate results
+        @studies.paginate(page: params[:page], per_page: Study.per_page)
       end
 
       swagger_path '/search/facets' do
@@ -169,6 +207,31 @@ module Api
         @search_facet = SearchFacet.find_by(identifier: params[:facet])
       end
 
+      def set_search_facets_and_filters
+        @facets = []
+        facet_queries = params[:facets].split('+')
+        facet_queries.each do |query|
+          facet_id, raw_filters = query.split(':')
+          filter_values = raw_filters.split(',')
+          facet = SearchFacet.find_by(identifier: facet_id)
+          if facet.present?
+            matching_filters = []
+            facet.filters.each do |filter|
+              if filter_values.include?(filter[:id])
+                matching_filters << filter
+              end
+            end
+            if matching_filters.any?
+              @facets << {
+                  id: facet.identifier,
+                  filters: matching_filters,
+                  object_id: facet.id # used for lookup later in :generate_bq_query_string
+              }
+            end
+          end
+        end
+      end
+
       # sanitize search values
       def sanitize_search_values(terms)
         if terms.is_a?(Array)
@@ -177,6 +240,29 @@ module Api
         else
           view_context.sanitize(terms)
         end
+      end
+
+      # generate query string for BQ
+      def generate_bq_query_string(facets)
+        base_query = "SELECT DISTINCT study_accession FROM #{CellMetadatum::BIGQUERY_TABLE} WHERE "
+        sub_queries = []
+        facets.each do |facet_obj|
+          # get the facet instance in order to run query
+          search_facet = SearchFacet.find(facet_obj[:object_id])
+          column_name = search_facet.big_query_id_column
+          if search_facet.is_array_based?
+            filter_values = facet_obj[:filters].map {|filter| filter[:id]}
+            unnest_queries = []
+            filter_values.each do |value|
+              unnest_queries << "'#{value}' IN UNNEST(#{column_name})"
+            end
+            sub_queries << "(#{unnest_queries.join(" OR ")})"
+          else
+            filter_values = facet_obj[:filters].map {|filter| filter[:id]}
+            sub_queries << "#{column_name} IN ('#{filter_values.join('\',\'')}')"
+          end
+        end
+        base_query += sub_queries.join(" AND ")
       end
     end
   end
--- app/views/api/v1/search/index.json.jbuilder
+++ app/views/api/v1/search/index.json.jbuilder
@@ -1,6 +1,11 @@
 json.set! :type, params[:type]
 json.set! :terms, params[:terms]
-json.set! :facets, params[:facets]
+json.facets do
+  json.array! @facets do |facet|
+    json.set! :id, facet[:id]
+    json.set! :filters, facet[:filters]
+  end
+end
 json.studies do
   json.array! @studies, partial: 'api/v1/site/study', as: :study
 end
