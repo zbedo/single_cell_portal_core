c42a784458fa1db791a0827596ae5da7526dbad7
Eric Weitz <eweitz@broadinstitute.org>
2020-02-11 13:44:49 -0500

    Update SearchPanel component to reflect renaming

--- app/javascript/components/SearchPanel.js
+++ app/javascript/components/SearchPanel.js
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import FacetControl from './FacetControl';
 import MoreFacetsButton from './MoreFacetsButton';
 import DownloadButton from './DownloadButton';
 
-// Only for development!  We'll fetch data once API endpoints are available.
-import {facetsResponseMock, searchFiltersResponseMock} from './FacetsMockData';
-const facets = facetsResponseMock;
+import { fetchFacets } from './../lib/scp-api';
 
 const defaultFacetIDs = ['disease', 'organ', 'species', 'cell_type'];
 const moreFacetIDs = ['sex', 'race', 'library_preparation_protocol', 'organism_age'];
-
-const defaultFacets = facets.filter(facet => defaultFacetIDs.includes(facet.id));
-const moreFacets = facets.filter(facet => moreFacetIDs.includes(facet.id));
-
-window.searchFiltersResponse = searchFiltersResponseMock;
 
 /**
  * Component for SCP advanced search UI
@@ -22,7 +15,22 @@ window.searchFiltersResponse = searchFiltersResponseMock;
  * This is the entry point into React code from the traditional JS code
  * See related integration at /app/javascript/packs/application.js
  */
-function ScpSearchStudies() {
+export default function SearchPanel() {
+
+  const [defaultFacets, setDefaultFacets] = useState([]);
+  const [moreFacets, setMoreFacets] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const facets = await fetchFacets(true);
+      const df = facets.filter(facet => defaultFacetIDs.includes(facet.id));
+      const mf = facets.filter(facet => moreFacetIDs.includes(facet.id));
+      setDefaultFacets(df);
+      setMoreFacets(mf);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div id='search-panel'>
       {
@@ -35,5 +43,3 @@ function ScpSearchStudies() {
     </div>
   );
 }
-
-export default ScpSearchStudies;
