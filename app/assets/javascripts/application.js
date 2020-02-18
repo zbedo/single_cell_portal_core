02507876c7aeb07f8f66d9aec6dfe9a04a107db5
Eric Weitz <eweitz@broadinstitute.org>
2020-02-18 17:33:06 -0500

    Merge changes from upstream

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1047,3 +1047,23 @@ function reopenUiTab(navTarget) {
         $(navTarget + ' a[href="' + tab + '"]').tab('show');
     }
 }
+
+// re-render a plot after a user selects a new cluster from the dropdown menu, usually called from a complete() callback
+// in an $.ajax() function
+function renderWithNewCluster(updateStatusText, renderCallback, setAnnotation=true) {
+    if (updateStatusText === 'success') {
+        if (setAnnotation) {
+            var an = $('#annotation').val();
+            $('#search_annotation').val(an);
+            $('#gene_set_annotation').val(an);
+        }
+        renderCallback();
+    }
+}
+
+// extract out identifier for global gene search by trimming off last -cluster, -annotation, or -subsample
+function extractIdentifierFromId(domId) {
+    var idParts = domId.split('-');
+    idParts.pop();
+    return idParts.join('-');
+}
