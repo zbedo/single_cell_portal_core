fe576bcfef4def233bd77eb5608fa8a87da7c293
Eno Akpan <eaugusti@broadinstitute.org>
2020-02-24 10:17:36 -0500

    Create homepage component

---
+++ app/javascript/components/HomePageContent.js
@@ -0,0 +1,32 @@
+import React, { Component, useState, useEffect } from 'react';
+import SearchPanel from './SearchPanel';
+import ResultsPanel from './ResultsPanel';
+
+
+export default function HomePageContent(){
+    const [results, setResults] = useState('');
+    const [keyword, setKeyword] = useState('');
+    const [type] = useState('study');
+    const [facets] = useState({});
+
+    const handleKeywordUpdate = (keyword) => {
+        setKeyword(keyword)
+            }
+
+    useEffect( () => {
+       fetch(`http://localhost:3000/single_cell/api/v1/search?type=${type}&terms=${keyword}`, {
+                headers: {
+                    'Accept': 'application/json',
+                }})
+                  .then((studyResults)=>{
+                                return studyResults.json()
+                            }).then(data =>setResults(data))}, [keyword]);
+
+    return (
+        <div>
+            <SearchPanel updateKeyword={handleKeywordUpdate}/>
+            {results && <ResultsPanel results={results}/>}
+        </div>
+    )
+}
+
