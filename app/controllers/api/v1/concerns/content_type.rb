fb303f91ce8a72755cd67b4f8b8084ac9f3f40a3
bistline <bistline@broadinstitute.org>
2020-02-26 17:22:34 -0500

    simplifying validate_content_type! to allow easier debugging via browser

--- app/controllers/api/v1/concerns/content_type.rb
+++ app/controllers/api/v1/concerns/content_type.rb
@@ -9,12 +9,13 @@ module Api
         end
 
         # default to JSON responses, disallow other Accept content types or format requests
-        # will allow Accept: */* and respond with JSON
+        # will allow */* or application/json in any part of Accept header and respond with JSON
         def validate_content_type!
-          accept_header = request.headers['Accept'].present? ? request.headers['Accept'] : 'application/json'
-          request_format = request.format.present? ? request.format : :json
-          if !%w(*/* application/json).include?(accept_header) || request_format != :json
+          accept_header = request.headers['Accept'].present? ? request.headers['Accept'] : ''
+          if !accept_header.include?("*/*") && !accept_header.include?('application/json')
             head 406
+          else
+            request.format = :json # override format to force JSON response
           end
         end
       end
