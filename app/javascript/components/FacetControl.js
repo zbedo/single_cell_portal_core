b3d715aebf2fa4f9c7a2ef9a78929103ee248f74
Devon Bush <dbush@broadinstitute.org>
2020-02-27 17:35:51 -0500

    modals close on clicks outside the container

--- app/javascript/components/FacetControl.js
+++ app/javascript/components/FacetControl.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FiltersBox from './FiltersBox';
 
 /**
@@ -16,18 +16,37 @@ export default function FacetControl(props) {
   const facetName = props.facet.name;
   const facetId = `facet-${slug(facetName)}`;
 
-  function handleClick() {
+  function handleButtonClick() {
     setShowFilters(!showFilters);
   }
+  // add event listener to detect mouseclicks outside the modal, so we know to close it
+  // see https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleOtherClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleOtherClick);
+    };
+  }, []);
+
+  const node = useRef()
+  const handleOtherClick = e => {
+    if (node.current.contains(e.target)) {
+      // click was inside the modal, do nothing
+      return;
+    }
+    setShowFilters(false)
+  };
 
   return (
-      <span
+      <span ref={node}
         id={facetId}
         className={`facet ${showFilters ? 'active' : ''}`}>
-        <span
-          onClick={handleClick}>
+        <a
+          onClick={handleButtonClick}>
           {facetName}
-        </span>
+        </a>
         <FiltersBox show={showFilters} facet={props.facet} />
       </span>
     );
--- app/javascript/components/MoreFacetsButton.js
+++ app/javascript/components/MoreFacetsButton.js
@@ -11,9 +11,9 @@ import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
  * UI spec: https://projects.invisionapp.com/d/main#/console/19272801/402387756/preview
  */
 export default function MoreFacetsButton(props) {
-  
+
   const [show, setShow] = useState(false);
-  
+
   // const facetName = props.facet.name;
 
   function handleClick() {
@@ -24,11 +24,11 @@ export default function MoreFacetsButton(props) {
       <span
         id='more-facets-button'
         className={`${show ? 'active' : ''} facet`}>
-        <span
+        <a
           onClick={handleClick}>
           <FontAwesomeIcon className="icon-left" icon={faSlidersH}/>
           More Facets
-        </span>
+        </a>
         {/* <FacetsAccordionBox show={show} facets={props.facets} /> */}
       </span>
     );
