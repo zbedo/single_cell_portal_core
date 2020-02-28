36fafe349890607b99effa53eae2af7405ff32aa
Eric Weitz <eweitz@broadinstitute.org>
2020-02-28 12:30:20 -0500

    Remove outdated comment, extraneous container

--- app/javascript/components/FacetsAccordion.js
+++ app/javascript/components/FacetsAccordion.js
@@ -7,30 +7,26 @@ import FiltersBox from './FiltersBox'
 export default function FacetsAccordion(props) {
 
   return (
-    // Accordions provide a way to restrict Card components to only open one at a time.
-    // https://react-bootstrap.github.io/components/accordion/
-    <div id='facets-accordion'>
-      <PanelGroup accordion>
-        {
-          props.facets.map((facet, i) => {
-            return (
-              <Panel key={i} eventKey={i}>
-                <Panel.Heading >
-                  <Panel.Title toggle>
-                    {facet.name}
-                  </Panel.Title>
-                </Panel.Heading>
-                <Panel.Body collapsible>
-                  <FiltersBox
-                    facet={facet}
-                    filters={facet.filters}
-                  />
-                </Panel.Body>
-              </Panel>
-            );
-          })
-        }
-      </PanelGroup>
-    </div>
+    <PanelGroup accordion id='facets-accordion'>
+      {
+        props.facets.map((facet, i) => {
+          return (
+            <Panel key={i} eventKey={i}>
+              <Panel.Heading >
+                <Panel.Title toggle>
+                  {facet.name}
+                </Panel.Title>
+              </Panel.Heading>
+              <Panel.Body collapsible>
+                <FiltersBox
+                  facet={facet}
+                  filters={facet.filters}
+                />
+              </Panel.Body>
+            </Panel>
+          );
+        })
+      }
+    </PanelGroup>
   );
 }
