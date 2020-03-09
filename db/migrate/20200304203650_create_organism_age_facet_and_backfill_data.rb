4c3b606c29eddc778e75ae43e51a96bd5c27256c
bistline <bistline@broadinstitute.org>
2020-03-09 10:05:53 -0400

    addressing PR comments

--- db/migrate/20200304203650_create_organism_age_facet_and_backfill_data.rb
+++ db/migrate/20200304203650_create_organism_age_facet_and_backfill_data.rb
@@ -4,7 +4,7 @@ class CreateOrganismAgeFacetAndBackfillData < Mongoid::Migration
     bq_dataset = SearchFacet.big_query_dataset
     update_command = "UPDATE #{CellMetadatum::BIGQUERY_TABLE} "
     update_command += "SET organism_age__seconds = CAST((organism_age * #{SearchFacet::TIME_MULTIPLIERS['years']}) AS NUMERIC) "
-    update_command += "WHERE organism_age IS NOT NULL and organism_age__seconds IS NULL"
+    update_command += "WHERE organism_age IS NOT NULL and organism_age__seconds IS NULL AND organism_age__unit_label = 'year'"
     bq_dataset.query update_command
     SearchFacet.create(name: 'Organism Age', identifier: 'organism_age', big_query_id_column: 'organism_age',
                        big_query_name_column: 'organism_age', big_query_conversion_column: 'organism_age__seconds',
