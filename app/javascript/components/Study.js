fd4f3760bb4dd2fb3d8ce873a63c122ffbff83d8
Eno Akpan <eaugusti@broadinstitute.org>
2020-03-19 09:17:34 -0400

    Fix opening text logic

--- app/javascript/components/Study.js
+++ app/javascript/components/Study.js
@@ -39,6 +39,7 @@ function shortenDescription(textDescription, term) {
     const firstIndex = matchesOutSideDescriptionWordLimit[0]
     // Find matches that fit within the n+descriptionWordLimit
     const ranges = matchesOutSideDescriptionWordLimit.filter(index => index < descriptionWordLimit+firstIndex)
+    // Determine where start and end index to ensure matched keywords are included
     const start = ((matchedIndicies.length- matchesOutSideDescriptionWordLimit.length)*(lengthOfHighlightTag+term.length)) +firstIndex
     const end = start + descriptionWordLimit + (ranges.length*(lengthOfHighlightTag+term.length))
     const descriptionText = styledText.slice(start-100, end)
@@ -47,12 +48,12 @@ function shortenDescription(textDescription, term) {
     const amountOfMatchesInSummaryWordLimit = matchedIndicies.filter(matchedIndex => matchedIndex <= summaryWordLimit).length
     if (amountOfMatchesInSummaryWordLimit>0) {
       //  Need to recaluculate index positions because added html changes size of textDescription
-      const summaryEndIndex= (amountOfMatchesInSummaryWordLimit *(lengthOfHighlightTag+term))
-      var summaryText = styledText.slice(0, summaryEndIndex)
-      return <><span>{summaryText}</span> <span className="detail">...</span><span dangerouslySetInnerHTML={displayedStudyDescription}></span>{suffixTag}</>
+      const beginningTextIndex= (amountOfMatchesInSummaryWordLimit *(lengthOfHighlightTag+term.length))
+      const displayedBeginningText = { __html: styledText.slice(0, beginningTextIndex+summaryWordLimit) }
+      return <><span dangerouslySetInnerHTML={displayedBeginningText}></span> <span className="detail">... </span><span dangerouslySetInnerHTML={displayedStudyDescription}></span>{suffixTag}</>
     }
-    var summaryText = styledText.slice(0, summaryWordLimit)
-    return <><span>{summaryText} </span><span className="detail">...</span> <span dangerouslySetInnerHTML={displayedStudyDescription}></span>{suffixTag}</>
+    const displayedBeginningText = styledText.slice(0, summaryWordLimit)
+    return <><span>{displayedBeginningText} </span><span className="detail">... </span> <span dangerouslySetInnerHTML={displayedStudyDescription}></span>{suffixTag}</>
   }
   const displayedStudyDescription = { __html: styledText.slice(0, descriptionWordLimit) }
   if (textDescription.length>descriptionWordLimit) {
