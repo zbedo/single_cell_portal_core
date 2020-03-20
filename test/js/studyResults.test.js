4fe20a23e915e8e6435f684fb6e249360a147089
Eno Akpan <eaugusti@broadinstitute.org>
2020-03-20 14:14:28 -0400

    Add tests for ResultsPanel

--- test/js/studyResults.test.js
+++ test/js/studyResults.test.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import StudyResultsContainer, { StudiesResults, StudiesList, Study } from '../../app/javascript/components/StudyResults'
+import StudyResultsContainer, { StudiesResults } from '../../app/javascript/components/StudyResults'
 import { shallow } from 'enzyme'
 import Tab from 'react-bootstrap/lib/Tab'
-import { useTable, usePagination } from 'react-table'
 
-const fetch = require('node-fetch')
 describe('<StudyResultsContainer/> rendering>', () => {
   let studyResultsContainer
   beforeAll(() => {
@@ -22,14 +20,3 @@ describe('<StudyResultsContainer/> rendering>', () => {
     expect(studyResultsContainer.find(StudiesResults)).toHaveLength(1)
   })
 })
-
-// describe('<StudiesList/> returning data correctly', () => {
-//   const studyMock = jest.mock(Study, () => {
-//     return <div/>
-//   })
-//   const mockList = { studies: [1, 2, 3, 4, 5] }
-//   const studiesList = shallow(<StudiesList studies={mockList}/>)
-//   it('should return list of 5 {study: <Study/>', () => {
-//     expect(studiesList).toHaveLength(5)
-//   })
-// })
