ff2a5f519490ebdba81e6e90dd6ca5ca1581bb27
bistline <bistline@broadinstitute.org>
2020-03-26 12:51:16 -0400

    updating integration test for updated logic

--- test/api/search_controller_test.rb
+++ test/api/search_controller_test.rb
@@ -349,17 +349,34 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
   test 'should run preset search' do
     puts "#{File.basename(__FILE__)}: #{self.method_name}"
 
-    @preset_search = PresetSearch.first
-    convention_study = Study.find_by(name: "Test Study #{@random_seed}")
+    # run whitelist only search
+    @preset_search = PresetSearch.create!(name: 'Preset Search Test', accession_whitelist: %w(SCP1))
     whitelisted_study = Study.first
     execute_http_request(:get, api_v1_search_path(type: 'study', preset_search: @preset_search.identifier))
     assert_response :success
-    preset_accessions = [whitelisted_study.accession, convention_study.accession]
-    assert json['matching_accessions'] == preset_accessions,
-           "Did not return correct studies for preset search; expected #{preset_accessions} but found #{json['matching_accessions']}"
+    whitelist_accessions = %w(SCP1)
+    assert json['matching_accessions'] == whitelist_accessions,
+           "Did not return correct studies for whitelisted preset search; expected #{whitelist_accessions} but found #{json['matching_accessions']}"
     found_preset = json['studies'].first
-    assert whitelisted_study.accession == @preset_search.accession_whitelist.first
+    assert found_preset['accession'] == whitelisted_study.accession
     assert found_preset['preset_match'], "Did not correctly mark whitelisted study as preset match; #{found_preset['preset_match']}"
+
+    # update to use user-search terms as well, include all studies to widen search
+    all_accessions = Study.pluck(:accession)
+    @preset_search.update(accession_whitelist: all_accessions)
+    @preset_search.reload
+    search_terms = "\"API Test Study\""
+    execute_http_request(:get, api_v1_search_path(type: 'study', preset_search: @preset_search.identifier, terms: search_terms))
+    assert_response :success
+    api_test_study = Study.find_by(name: /API Test Study/)
+    assert_equal 1, json['studies'].count, "Found wrong number of studies; should be 1 but found #{json['studies'].count}"
+    expected_results = [api_test_study.accession]
+    assert_equal expected_results, json['matching_accessions'],
+                 "Found wrong study with keywords & preset search; expected #{expected_results} but found #{json['matching_accessions']}"
+    found_study = json['studies'].first
+    assert found_study['preset_match']
+    assert_equal ['API Test Study'], found_study['term_matches'], "Did not correctly match on #{search_terms}: #{found_study['term_matches']}"
+    @preset_search.destroy # clean up
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
