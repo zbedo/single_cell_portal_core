1ae2132e3011e0f693456e74333225be8eda4145
Eno Akpan <eaugusti@broadinstitute.org>
2020-03-31 14:46:21 -0400

    Add tests

--- test/js/search/search-query-display.test.js
+++ test/js/search/search-query-display.test.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import * as ReactAll from 'react';
-import { mount } from 'enzyme';
+import React from 'react'
+import { mount } from 'enzyme'
 
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
-import SearchQueryDisplay from 'components/SearchQueryDisplay';
+import SearchQueryDisplay, { ClearAllButton } from 'components/SearchQueryDisplay'
+import { PropsStudySearchProvider } from 'components/search/StudySearchProvider'
+import KeywordSearch from 'components/KeywordSearch'
+import FacetControl from 'components/FacetControl'
+
 
 const oneStringFacet = [
-  {id: 'species', filters: [{id: "NCBITaxon_9606", name: "Homo sapiens"}]}
+  { id: 'species', filters: [{ id: 'NCBITaxon_9606', name: 'Homo sapiens' }] }
 ]
 
 const twoStringFacets = [
-  {id: 'disease', filters: [{id: "id1", name: "disease1"}]},
-  {id: 'species', filters: [{id: "NCBITaxon_9606", name: "Homo sapiens"}]}
+  { id: 'disease', filters: [{ id: 'id1', name: 'disease1' }] },
+  { id: 'species', filters: [{ id: 'NCBITaxon_9606', name: 'Homo sapiens' }] }
 ]
 
 const stringAndNumericFacets = [
-  {id: 'species', filters: [
-    {id: "NCBITaxon_9606", name: "Homo sapiens"},
-    {id: "NCBITaxon_10090", name: "Mus musculus"}
-  ]},
-  {id: "organism_age", filters: {min: 14, max: 180, unit: "years"}}
+  {
+    id: 'species', filters: [
+      { id: 'NCBITaxon_9606', name: 'Homo sapiens' },
+      { id: 'NCBITaxon_10090', name: 'Mus musculus' }
+    ]
+  },
+  { id: 'organism_age', filters: { min: 14, max: 180, unit: 'years' } }
 ]
 
 describe('Search query display text', () => {
@@ -28,34 +33,64 @@ describe('Search query display text', () => {
     const wrapper = mount((
       <SearchQueryDisplay facets={oneStringFacet} terms={''}/>
     ))
-    expect(wrapper.text().trim()).toEqual(': Metadata contains (species: Homo sapiens)')
+    expect(wrapper.text().trim()).toEqual(': Metadata contains (species: Homo sapiens) Clear All')
   })
 
   it('renders multiple facets', async () => {
     const wrapper = mount((
       <SearchQueryDisplay facets={twoStringFacets} terms={''}/>
     ))
-    expect(wrapper.text().trim()).toEqual(': Metadata contains (disease: disease1) AND (species: Homo sapiens)')
+    expect(wrapper.text().trim()).toEqual(': Metadata contains (disease: disease1) AND (species: Homo sapiens) Clear All')
   })
 
   it('renders string and numeric facets', async () => {
     const wrapper = mount((
       <SearchQueryDisplay facets={stringAndNumericFacets} terms={''}/>
     ))
-    expect(wrapper.text().trim()).toEqual(': Metadata contains (species: Homo sapiens OR Mus musculus) AND (organism age: 14 - 180 years)')
+    expect(wrapper.text().trim()).toEqual(': Metadata contains (species: Homo sapiens OR Mus musculus) AND (organism age: 14 - 180 years) Clear All')
   })
 
   it('renders terms', async () => {
     const wrapper = mount((
       <SearchQueryDisplay facets={[]} terms={['foo']}/>
     ))
-    expect(wrapper.text().trim()).toEqual(': Text contains (foo)')
+    expect(wrapper.text().trim()).toEqual(': Text contains (foo) Clear All')
   })
 
   it('renders terms and a single facet', async () => {
     const wrapper = mount((
       <SearchQueryDisplay facets={oneStringFacet} terms={['foo', 'bar']}/>
     ))
-    expect(wrapper.text().trim()).toEqual(': (Text contains (foo OR bar)) AND (Metadata contains (species: Homo sapiens))')
+    expect(wrapper.text().trim()).toEqual(': (Text contains (foo OR bar)) AND (Metadata contains (species: Homo sapiens)) Clear All')
+  })
+})
+
+describe('Clearing search query', () => {
+  it('clears search params', () => {
+    const speciesFacet = {
+      name: 'Species',
+      id: 'species',
+      type: 'string',
+      links: [{ name: 'NCBI Taxonomy', url: 'https://foo.tdb' }],
+      filters: [
+        { id: 'NCBITaxon_9606', name: 'Homo Sapiens' }
+      ],
+      links: []
+    }
+    const component = <PropsStudySearchProvider searchParams={{ terms: 'foo', facets: { species: ['NCBITaxon_9606'] } }}>
+      <ClearAllButton/>
+      <KeywordSearch/>
+      <FacetControl facet={speciesFacet}/>
+    </PropsStudySearchProvider>
+    const wrapper = mount(component)
+    // wrapper.find('.badge').text()
+    wrapper.find('#facet-species > a').simulate('click')
+    console.log(wrapper.debug())
+    wrapper.find(ClearAllButton).simulate('click')
+    expect(wrapper.find('input[name="keywordText"]').first().props().value).toEqual('')
+    // Check if badge for filter doesn't exist
+    expect(wrapper.find('.filter-badge-list')).toHaveLength(0)
+    // Filter should not be checked
+    expect(wrapper.find('input[name="NCBITaxon_9606"]').props().checked).toEqual(false)
   })
 })
