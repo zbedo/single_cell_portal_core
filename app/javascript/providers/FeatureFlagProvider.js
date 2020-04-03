120007cbbe89902c11d81ca45fab9ebe35a5b51a
Devon Bush <dbush@broadinstitute.org>
2020-04-02 20:44:28 -0400

    cleaning up minor lint issues

--- app/javascript/providers/FeatureFlagProvider.js
+++ app/javascript/providers/FeatureFlagProvider.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 export function getAllFlags() {
   // for now, read it off the home page.  Eventually, this will want to be an API call
--- app/javascript/providers/SearchSelectionProvider.js
+++ app/javascript/providers/SearchSelectionProvider.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState } from 'react'
 import { StudySearchContext } from './StudySearchProvider'
 import _clone from 'lodash/clone'
 
