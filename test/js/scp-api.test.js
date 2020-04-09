26b296ca55c65bebfbf3b775f09864bfce56ed83
Eno-Abasi Augustine-Akpan <37722472+knapii-developments@users.noreply.github.com>
2020-04-09 10:04:52 -0400

    Update test/js/scp-api.test.js
    Co-Authored-By: Eric Weitz <eweitz@broadinstitute.org>

--- test/js/scp-api.test.js
+++ test/js/scp-api.test.js
@@ -1,4 +1,4 @@
-// Without disabling eslint code is Promises are auto inserted
+// Without disabling eslint code, Promises are auto inserted
 /* eslint-disable*/
 
 const fetch = require('node-fetch')
