bda674dafb98c271f4c31a3cb6cf03cf956ed26d
Eno Akpan <eaugusti@broadinstitute.org>
2020-04-09 11:15:04 -0400

    Remove 500

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -153,7 +153,6 @@ module Api
       end
 
       def index
-        render json:{error: 'blah'}, status:500
         @viewable = Study.viewable(current_api_user)
 
         # filter results by branding group, if specified
