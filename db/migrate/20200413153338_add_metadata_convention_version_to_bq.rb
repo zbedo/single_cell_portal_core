915616aee6260783d5e1f6ae0a14332375943ee5
bistline <bistline@broadinstitute.org>
2020-04-13 13:06:10 -0400

    adding migrations for new BQ columns

---
+++ db/migrate/20200413153338_add_metadata_convention_version_to_bq.rb
@@ -0,0 +1,26 @@
+class AddMetadataConventionVersionToBq < Mongoid::Migration
+  def self.up
+    # do portal table first, then test dataset
+    client = BigQueryClient.new.client
+    [CellMetadatum::BIGQUERY_DATASET, 'cell_metadata_test'].each do |dataset_name|
+      dataset = client.dataset(dataset_name)
+      table = dataset.table(CellMetadatum::BIGQUERY_TABLE)
+      table.schema {|s| s.string('metadata_convention_version', mode: :nullable)}
+      update_command = "UPDATE #{CellMetadatum::BIGQUERY_TABLE} "
+      update_command += "SET metadata_convention_version = '1.1.3'"
+      update_command += "WHERE metadata_convention_version IS NULL"
+      dataset.query update_command
+    end
+  end
+
+  def self.down
+    client = BigQueryClient.new.client
+    [CellMetadatum::BIGQUERY_DATASET, 'cell_metadata_test'].each do |dataset_name|
+      dataset = client.dataset(dataset_name)
+      update_command = "UPDATE #{CellMetadatum::BIGQUERY_TABLE} "
+      update_command += "SET metadata_convention_version = NULL"
+      update_command += "WHERE metadata_convention_version = '1.1.3'"
+      dataset.query update_command
+    end
+  end
+end
