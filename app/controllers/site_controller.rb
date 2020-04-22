f84ee4169f986259186da4857c2cb1fb04f5bf63
bistline <bistline@broadinstitute.org>
2020-04-22 11:12:10 -0400

    putting @allow_computes back in to preserve tab behavior

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1448,25 +1448,27 @@ class SiteController < ApplicationController
     @allow_firecloud_access = false
     @allow_downloads = false
     @allow_edits = false
-    if !study_detached
-      begin
-        @allow_firecloud_access = AdminConfiguration.firecloud_access_enabled?
-        api_status = Study.firecloud_client.api_status
-        # reuse status object because firecloud_client.services_available? each makes a separate status call
-        # calling Hash#dig will gracefully handle any key lookup errors in case of a larger outage
-        if api_status.is_a?(Hash)
-          system_status = api_status['systems']
-          sam_ok = system_status.dig('Sam', 'ok') == true # do equality check in case 'ok' node isn't present
-          rawls_ok = system_status.dig('Rawls', 'ok') == true
-          buckets_ok = system_status.dig('GoogleBuckets', 'ok') == true
-          @allow_downloads = buckets_ok
-          @allow_edits = sam_ok && rawls_ok
-        end
-      rescue => e
-        logger.error "Error checking FireCloud API status: #{e.class.name} -- #{e.message}"
-        error_context = ErrorTracker.format_extra_context(@study, {firecloud_status: api_status})
-        ErrorTracker.report_exception(e, current_user, error_context)
+    @allow_computes = false
+    return if study_detached
+    begin
+      @allow_firecloud_access = AdminConfiguration.firecloud_access_enabled?
+      api_status = Study.firecloud_client.api_status
+      # reuse status object because firecloud_client.services_available? each makes a separate status call
+      # calling Hash#dig will gracefully handle any key lookup errors in case of a larger outage
+      if api_status.is_a?(Hash)
+        system_status = api_status['systems']
+        sam_ok = system_status.dig('Sam', 'ok') == true # do equality check in case 'ok' node isn't present
+        agora_ok = system_status.dig('Agora', 'ok')
+        rawls_ok = system_status.dig('Rawls', 'ok') == true
+        buckets_ok = system_status.dig('GoogleBuckets', 'ok') == true
+        @allow_downloads = buckets_ok
+        @allow_edits = sam_ok && rawls_ok
+        @allow_computes = sam_ok && rawls_ok && agora_ok
       end
+    rescue => e
+      logger.error "Error checking FireCloud API status: #{e.class.name} -- #{e.message}"
+      error_context = ErrorTracker.format_extra_context(@study, {firecloud_status: api_status})
+      ErrorTracker.report_exception(e, current_user, error_context)
     end
   end
 
