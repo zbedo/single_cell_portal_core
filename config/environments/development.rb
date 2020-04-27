8c44ccbfb1a23e3b95f195b9c1f6e45eac286a1f
Devon Bush <dbush@broadinstitute.org>
2020-04-27 11:44:57 -0400

    WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
     SwaggerUiEngine::SwaggerDocsController.send(:include, Api::V1::Concerns::CspHeaderBypass)
     SwaggerUiEngine::SwaggerDocsController.send(:layout, 'swagger_ui_engine/layouts/swagger')
   end
+
+  config.force_ssl = true
 end
