53d5105eccae779cdc169cbc39b3a0e62cbb2bff
Eric Weitz <eweitz@broadinstitute.org>
2020-05-01 16:20:37 -0400

    Deep-clean code in kernel-functions.js

--- app/assets/javascripts/kernel-functions.js
+++ app/assets/javascripts/kernel-functions.js
@@ -1,136 +1,154 @@
-/* eslint-disable */
-
-/* Advanced Rule of thumb bandwidth selector from :
-    https://en.wikipedia.org/wiki/Kernel_density_estimation#Bandwidth_selection
-    and https://stat.ethz.ch/R-manual/R-devel/library/stats/html/bandwidth.html
+/**
+ * Returns a normal reference distribution (nrd)
+ *
+ * Advanced Rule of thumb bandwidth selector from:
+ *  https://en.wikipedia.org/wiki/Kernel_density_estimation#Bandwidth_selection
+ *  and https://stat.ethz.ch/R-manual/R-devel/library/stats/html/bandwidth.html
 */
-
-function nrd0(X){
-    var iqr = jStat.percentile(X, 0.75) - jStat.percentile(X, 0.25);
-    var iqrM = iqr /1.34;
-    var std = jStat.stdev(X,true);
-    var min = std < iqrM ? std : iqrM;
-    if(min === 0){
-        min = std
-    }
-    if(min === 0){
-        min = Math.abs(X[1])
-    }
-    if(min === 0){
-        min = 1.0
-    }
-    return 0.9 * min * Math.pow(X.length, -0.2)
+function nrd0(X) {
+  const iqr = jStat.percentile(X, 0.75) - jStat.percentile(X, 0.25)
+  const iqrM = iqr /1.34
+  const std = jStat.stdev(X, true)
+  let min = std < iqrM ? std : iqrM
+  if (min === 0) {
+    min = std
+  }
+  if (min === 0) {
+    min = Math.abs(X[1])
+  }
+  if (min === 0) {
+    min = 1.0
+  }
+  return 0.9 * min * Math.pow(X.length, -0.2)
 }
 
-// More memory- and time-efficient analogs of Math.min and Math.max.
-// From https://stackoverflow.com/a/13440842/10564415.
+/**
+ * More memory- and time-efficient analog of Math.min
+ * From https://stackoverflow.com/a/13440842/10564415.
+*/
 function arrayMin(arr) {
-  var len = arr.length, min = Infinity;
+  let len = arr.length; let min = Infinity
   while (len--) {
     if (arr[len] < min) {
-      min = arr[len];
+      min = arr[len]
     }
   }
-  return min;
+  return min
 }
+
+/**
+ * More memory- and time-efficient analog of Math.max
+ * From https://stackoverflow.com/a/13440842/10564415.
+*/
 function arrayMax(arr) {
-  var len = arr.length, max = -Infinity;
+  let len = arr.length; let max = -Infinity
   while (len--) {
     if (arr[len] > max) {
-      max = arr[len];
+      max = arr[len]
     }
   }
-  return max;
+  return max
 }
 
-//This is the master function that creates all the plotly traces.
-//Takes an array of arrays and returns the data array of traces and the layout variable
-function createTracesAndLayout(arr, title, jitter='all', expressionLabel){
-    // Iterate through the formatted array [[name_of_trace, expression_data]...]
-    // and create the response plotly objects, returning [plotly data object, plotly layout object]
-    var data = Array();
-    for(x=0;x<arr.length;x++){
-        // plotly violin trace creation, adding to master array
-        // get inputs for plotly violin creation
-        var dist = arr[x][1];
-        var name = arr[x][0];
-        // if people want to change bandwidth eventually this is what we would change with a parameter
-        var bandwidth = nrd0(dist);
+/**
+ * Creates Plotly traces
+ * Takes an array of arrays and returns the data array of traces and the
+ * layout variable.  More specifically, this will:
+ *
+ * Iterate through the formatted array
+ * [[name_of_trace, expression_data]...]
+ * and create the response plotly objects,
+ * returning [plotly data object, plotly layout object]
+*/
+function createTracesAndLayout(
+  arr, title, jitter='all', expressionLabel
+) {
+  let data = []
+  for (let x = 0; x < arr.length; x++) {
+    // Plotly violin trace creation, adding to master array
+    // get inputs for plotly violin creation
+    const dist = arr[x][1]
+    const name = arr[x][0]
 
-        // replace the none selection with bool false for plotly
-        if (jitter === ''){
-            jitter = false;
-        }
-        // check if there is a distribution before adding trace
-        if ( arrayMax(dist) !== arrayMin(dist) ) {
-            // make a violin plot if there is a distribution
-            data = data.concat([{
-                type: 'violin',
-                name: name,
-                y: dist,
-                "points": jitter,
-                "pointpos": 0,
-                "jitter": .85,
-                "spanmode": 'hard',
-                box: {
-                    visible: true,
-                    fillcolor: '#ffffff',
-                    width: .1
-                },
-                bandwidth: bandwidth,
-                marker: {
-                    size: 2,
-                    color: '#000000',
-                    opacity: 0.8
-                },
-                fillcolor: colorBrewerSet[x % 27],
-                line: {
-                    color: '#000000',
-                    width: 1.5
-                },
-                meanline: {
-                    visible: false
-                }
-            }])
-        } else {
-            // Make a boxplot for data with no distribution
-            data = data.concat([{
-                type: 'box',
-                name: name,
-                y: dist,
-                boxpoints: jitter,
-                marker: {
-                    color: colorBrewerSet[x % 27],
-                    size: 2,
-                    line: {
-                        color: plotlyDefaultLineColor
-                    }
-                },
-                boxmean: true,
-            }])
-        }
+    // If users want to change bandwidth, we would parameterize this.
+    const bandwidth = nrd0(dist)
 
+    // Replace the none selection with bool false for plotly
+    if (jitter === '') {
+      jitter = false
     }
-    var layout = {
-        title: title,
-        // Force axis labels, including number strings, to be treated as
-        // categories.  See Python docs (same generic API as JavaScript):
-        // https://plotly.com/python/axes/#forcing-an-axis-to-be-categorical
-        // Relevant Plotly JS example:
-        // https://plotly.com/javascript/axes/#categorical-axes
-        xaxis: {
-          type: 'category'
+
+    // Check if there is a distribution before adding trace
+    if (arrayMax(dist) !== arrayMin(dist)) {
+      // Make a violin plot if there is a distribution
+      data = data.concat([{
+        'type': 'violin',
+        name,
+        'y': dist,
+        'points': jitter,
+        'pointpos': 0,
+        'jitter': 0.85,
+        'spanmode': 'hard',
+        'box': {
+          visible: true,
+          fillcolor: '#ffffff',
+          width: .1
         },
-        yaxis: {
-            zeroline: true,
-            showline: true,
-            title: expressionLabel
+        bandwidth,
+        'marker': {
+          size: 2,
+          color: '#000000',
+          opacity: 0.8
         },
-        margin: {
-            pad: 10,
-            b: 100
+        'fillcolor': colorBrewerSet[x % 27],
+        'line': {
+          color: '#000000',
+          width: 1.5
         },
-        autosize: true
-    };
-    return [data, layout]
+        'meanline': {
+          visible: false
+        }
+      }])
+    } else {
+      // Make a boxplot for data with no distribution
+      data = data.concat([{
+        type: 'box',
+        name,
+        y: dist,
+        boxpoints: jitter,
+        marker: {
+          color: colorBrewerSet[x % 27],
+          size: 2,
+          line: {
+            color: plotlyDefaultLineColor
+          }
+        },
+        boxmean: true
+      }])
+    }
+  }
+
+  const layout = {
+    title,
+    // Force axis labels, including number strings, to be treated as
+    // categories.  See Python docs (same generic API as JavaScript):
+    // https://plotly.com/python/axes/#forcing-an-axis-to-be-categorical
+    // Relevant Plotly JS example:
+    // https://plotly.com/javascript/axes/#categorical-axes
+    xaxis: {
+      type: 'category'
+    },
+    yaxis: {
+      zeroline: true,
+      showline: true,
+      title: expressionLabel
+    },
+    margin: {
+      pad: 10,
+      b: 100
+    },
+    autosize: true
+  }
+
+  return [data, layout]
 }
