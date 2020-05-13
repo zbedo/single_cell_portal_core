c9cfb6c2e3307f15437c740274955f54a869b273
Eric Weitz <eweitz@broadinstitute.org>
2020-05-13 09:36:34 -0400

    Indent with spaces, not tabs, per codebase convention

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -22,34 +22,40 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     bard_domain = bard_domains_by_env[Rails.env.to_sym]
     bard_path = bard_domain + '/api/identify'
-    Rails.logger.info bard_path
     headers = {'Authorization' => "Bearer #{@user.access_token['access_token']}"}
-    response = RestClient::Request.execute(
-      method: 'GET',
-      url: bard_path,
-      headers: headers,
-      payload: {'anonId': 'foo'}
-    )
+
+    begin
+      response = RestClient::Request.execute(
+        method: 'POST',
+        url: bard_path,
+        headers: headers,
+        payload: {'anonId': cookies['user_id']}
+      )
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.error "Bard error: #{e.message}"
+      Rails.logger.error e.response.to_yaml
+    end
+
   end
 
-	def google_oauth2
-		# You need to implement the method below in your model (e.g. app/models/user.rb)
-		@user = User.from_omniauth(request.env["omniauth.auth"])
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-		if @user.persisted?
-			@user.update(authentication_token: Devise.friendly_token(32))
-			@user.generate_access_token
-			# update a user's FireCloud status
-			@user.delay.update_firecloud_status
-			sign_in(@user)
+    if @user.persisted?
+      @user.update(authentication_token: Devise.friendly_token(32))
+      @user.generate_access_token
+      # update a user's FireCloud status
+      @user.delay.update_firecloud_status
+      sign_in(@user)
       if TosAcceptance.accepted?(@user)
         self.merge_identities
-				redirect_to request.env['omniauth.origin'] || site_path
-			else
-				redirect_to accept_tos_path(@user.id)
+        redirect_to request.env['omniauth.origin'] || site_path
+      else
+        redirect_to accept_tos_path(@user.id)
       end
-		else
-			redirect_to new_user_session_path
-		end
-	end
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
